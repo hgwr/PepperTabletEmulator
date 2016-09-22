0bc65ea681522034663a97945fc7042dcd193264
Shigeru Hagiwara <hgwrsgr@gmail.com>
2016-09-22 15:32:58 +0900

    *** empty log message ***

--- 01_run_naoqi-bin.sh
+++ 01_run_naoqi-bin.sh
@@ -3,6 +3,8 @@
 script_home="$(cd -P "$(dirname "$0")" && pwd)"
 . "${script_home}/sh_settings"
 
+cd "$CHOREGRAPHEDIR"
+
 DYLD_FRAMEWORK_PATH=Choregraphe.app/Contents/Resources \
 DYLD_LIBRARY_PATH=Choregraphe.app/Contents/Resources/lib \
 Choregraphe.app/Contents/Resources/bin/naoqi-bin
