62735d30d568c5d2020ef52615160f47b84ec66e
Shigeru Hagiwara <hgwrsgr@gmail.com>
2016-09-22 15:42:30 +0900

    fix script

--- 02_run_qimessaging-json.sh
+++ 02_run_qimessaging-json.sh
@@ -4,7 +4,7 @@ script_home="$(cd -P "$(dirname "$0")" && pwd)"
 . "${script_home}/sh_settings"
 
 there_is_naoqi_bin=`ps -aef | grep Choregraphe.app/Contents/Resources/bin/naoqi-bin | grep -v grep | wc -l`
-if [ "$there_is_naoqi_bin" != "1" ]; then
+if [ $there_is_naoqi_bin -ne 1 ]; then
     echo "Error: There are no naoqi-bin process."
     exit 1
 fi
