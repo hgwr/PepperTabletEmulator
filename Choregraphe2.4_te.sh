73ddbdb13dc63379b112804bbb85fd56dd349019
Shigeru Hagiwara <hgwrsgr@gmail.com>
2016-09-22 15:13:45 +0900

    *** empty log message ***

--- Choregraphe2.4_te.sh
+++ Choregraphe2.4_te.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-NAOQISDK="/Users/hgwr/lib/pynaoqi-python2.7-2.1.4.13-mac64" # <--- NAOqi Python SDK を保存した場所で書き換える
+# NAOqi Python SDK を保存した場所
+NAOQISDK="$HOME/lib/pynaoqi-python2.7-2.1.4.13-mac64"
 
-CHOREGRAPHEDIR="/Applications/Aldebaran Robotics/Choregraphe Suite 2.1" # <-- Choregraphe のインストールディレクトリ
+# Choregraphe のインストールディレクトリ
+CHOREGRAPHEDIR="/Applications/Aldebaran Robotics/Choregraphe Suite 2.1" 
+
 
 cd "$CHOREGRAPHEDIR"
 
@@ -19,25 +22,3 @@ export PYTHONPATH="$NAOQISDK":$PYTHONPATH
 /usr/bin/python /usr/local/bin/PepperTabletEmulator.py &
 
 open --wait-apps ./Choregraphe.app --args -p 9559 
-
-kill %1
-kill %2
-kill %3
-
-sleep 5
-
-echo ""
-echo "終了しました！"
-echo "まれにバックグラウンドプロセスが残っている場合があります"
-echo "プロセスが残っていると次回の Choregraphe の起動時"
-echo "バーチャルロボットがうまく機能しないことがあります"
-echo "おかしいなと思ったら、一旦 Choregrape を停止"
-echo "ps -ef コマンドで naoqi-bin と qimessaging-json "
-echo "というプロセスが動いていないか確認、動いていたら kill "
-echo "コマンドで停止させてください"
-echo ""
-
-
-
-
-
