c38bce73711fec627e25e0161de93b37bedadd4d
Shigeru Hagiwara <hgwrsgr@gmail.com>
2017-06-12 15:23:43 +0900

    add setup_env.sh

---
+++ fc/setup_env.sh
@@ -0,0 +1,5 @@
+export PYTHONPATH=${PYTHONPATH}:${HOME}/lib/pynaoqi-python2.7-2.4.3.28-mac64
+export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${HOME}/lib/pynaoqi-python2.7-2.4.3.28-mac64
+
+# export PYTHONPATH=${HOME}/lib/pynaoqi-python2.7-2.5.5.5-mac6:${PYTHONPATH}
+# export DYLD_LIBRARY_PATH=${HOME}/lib/pynaoqi-python2.7-2.5.5.5-mac6:${DYLD_LIBRARY_PATH}
